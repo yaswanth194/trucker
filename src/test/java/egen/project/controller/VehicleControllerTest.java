f9d4c705abce6f49a136a7c7c81e71d50e8cbef9
Yaswanth Reddy <yaswanth194@gmail.com>
2018-06-03 23:01:02 -0500

    Added Tester
    Added Tester to test Vehicle Controller

--- src/test/java/egen/project/controller/VehicleControllerTest.java
+++ src/test/java/egen/project/controller/VehicleControllerTest.java
@@ -1,5 +1,11 @@
 package egen.project.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import egen.project.Repository.VehicleRepository;
+import egen.project.entity.Vehicle;
+import org.hamcrest.Matchers;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +17,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.regex.Matcher;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
@@ -20,14 +31,69 @@ import static org.junit.Assert.*;
 public class VehicleControllerTest {
 
     @Autowired
+    ObjectMapper mapper;
+
+    @Autowired
     MockMvc mvc;
 
+    @Autowired
+    VehicleRepository vehicleRepository;
+
+    @After
+    public void breakDown(){
+        vehicleRepository.deleteAll();
+    }
+
     @Test
-    public void printController() {
+    public void printController() throws Exception {
+        Vehicle veh= new Vehicle();
+        veh.setVin("1HGCR2F3XFA027534");
+        veh.setMake("HONDA");
+        veh.setModel("ACCORD");
+        veh.setYear(2005);
+        veh.setRedlineRpm(5500);
+        veh.setMaxFuelVolume(15);
+        Calendar calendar = Calendar.getInstance();
+        Date now =calendar.getTime();
+        veh.setLastServiceDate(new Timestamp(now.getTime()));
+
+        vehicleRepository.save(veh);
+        mvc.perform(MockMvcRequestBuilders.get("/vehicles/")).andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0].make",Matchers.is("HONDA")));
     }
 
     @Test
     public void getAllVehicles() throws Exception {
-        //mvc.perform(MockMvcRequestBuilders.get()).andExpect(MockMvcResultMatchers.status().isOk());
+        mvc.perform(MockMvcRequestBuilders.get("/vehicles/")).andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    public void testPost() throws Exception{
+        Vehicle[] each;
+        each= new Vehicle[2];
+        Vehicle veh= new Vehicle();
+        veh.setVin("1HGCR2F3XFA027534");
+        veh.setMake("HONDA");
+        veh.setModel("ACCORD");
+        veh.setYear(2005);
+        veh.setRedlineRpm(5500);
+        veh.setMaxFuelVolume(15);
+        Calendar calendar = Calendar.getInstance();
+        Date now =calendar.getTime();
+        veh.setLastServiceDate(new Timestamp(now.getTime()));
+
+        Vehicle veh1= new Vehicle();
+        veh.setVin("1HGCR2F3XFA027534");
+        veh.setMake("HONDA");
+        veh.setModel("ACCORD");
+        veh.setYear(2005);
+        veh.setRedlineRpm(5500);
+        veh.setMaxFuelVolume(15);
+        veh.setLastServiceDate(new Timestamp(now.getTime()));
+
+        each[0]=veh;
+        each[1]=veh1;
+        mvc.perform(MockMvcRequestBuilders.put("/vehicles").content(mapper.writeValueAsBytes(each)))
+                .andExpect(MockMvcResultMatchers.status().isUnsupportedMediaType());
     }
 }
